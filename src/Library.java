b2ff4121b382474db43aea61f0b217201796a82c
BISWAROOP <biswaroop0110@gmail.com>
2021-10-12 00:22:22 +0530

    pls check

--- src/Library.java
+++ src/Library.java
@@ -1,4 +1,5 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.Objects;
 
 public class Library
 {
@@ -44,85 +45,107 @@ public class Library
         this.bookName=bookName;
         this.author=author;
     }
-    static HashMap<Integer,Library> books= new HashMap<Integer, Library>();
-    static HashMap<Integer,Library> bookcopy= new HashMap<Integer, Library>();
-    static HashMap<Integer, Integer> borrowers=new HashMap<Integer,Integer>();
-
+    static HashMap<Integer,Library> books= new HashMap<>();
+    static HashMap<Integer,Library> bookcopy= new HashMap<>();
+    static HashMap<Integer, Integer> borrowers= new HashMap<>();
+    int[] borrow = new int[1000];
+    String[] book = new String[999];
     public HashMap<Integer,Library> createLibraryMap(String booksInLibrary)
     {   String S=booksInLibrary.replace('|','/');
-        String s[]=S.split("/");
+        String[] s =S.split("/");
+        int i =0;
         for(String x: s) {
-            String q[]=x.split(",");
+            String[] q =x.split(",");
             Library li = new Library(q[1],q[2]);
             books.put(Integer.parseInt(q[0]),li);
+            book[i] = q[0];
+            book[i+1] = q[1];
+            book[i+2] = q[2];
+            i=i+3;
         }
         bookcopy .putAll(books);
-        System.out.println(bookcopy); System.out.println(books);
         return books;
     }
 
     public HashMap<Integer,Integer> createUserMap(String borrowedUsers)
     {   String S=borrowedUsers.replace('|','/');
-        String s[]=S.split("/");
+        String[] s =S.split("/");
+        int i=0;
         for(String x: s) {
-            String q[]=x.split(",");
+            String[] q=x.split(",");
             books.remove(Integer.parseInt(q[0]));
             borrowers.put(Integer.parseInt(q[0]),Integer.parseInt(q[1]));
+            borrow[i] = Integer.parseInt(q[1]);
+            borrow[i+1] = Integer.parseInt(q[0]);
+            i=i+2;
         }
-        System.out.println(borrowers);
         return borrowers;
     }
 
     public String getQuery(String booksInLibrary,String borrowedUsers,String query)
-    {   if(query=="1") {
-        int id=Integer.parseInt(borrowedUsers);
-        if(books.containsKey(id)) {
-            System.out.println(books.get(id));
-        }else {
-            System.out.println("No stock");
-        }
-        if(borrowers.containsKey(id)) {
-            System.out.println("It is owned by "+borrowers.get(id));
-        }
-    }else if(query=="2") {
-        int id=Integer.parseInt(borrowedUsers);
-        if(borrowers.containsValue(id)) {
-            System.out.printf(id+", "+ bookcopy.get(id).bookName);
-        }
-    }else if(query=="3") {
-        int count1=0,count2=0;
-        for(Integer x: books.keySet())
-            {
-                ++count1;
-            }
-        for(Integer x:borrowers.keySet())
-            {
-                ++count2;
-            }
-        System.out.println(count2+" out");
-        System.out.println(count1+" in");
-    }else if(query=="4") {
-        for(Library x:books.values()) {
-            String s0 = x.bookName;
-            String s1 = x.author;
-            if(s1.equals(borrowedUsers)) {
-                System.out.println(s0);
-            }
-        }
-    }else if(query=="5"){
-        for(Integer x: books.keySet()) {
-            Library lx = books.get(x);
-            String S0=lx.bookName;
-            String S1= lx.author;
-            if(S0.contains(borrowedUsers)) {
-                System.out.println(x+" "+S0);
-            }
-        }
-    }
+    {
+        switch (query) {
+            case "1":
+                int id = Integer.parseInt(borrowedUsers);
+                if (books.containsKey(id)) {
+                    Library l1 = books.get(id);
+                    System.out.print("It is available\n"+l1.author+"\n");
+                } else {
+                    System.out.print("No stock\nIt is owned by " + borrowers.get(id)+"\n");
+                }
 
+                break;
+            case "2":
+                int uid = Integer.parseInt(borrowedUsers);
+                for (int i = 0;  i < ((borrowedUsers.length()) * 2);  i++) {
+                    if (uid == borrow[i]) {
+                        int bid = borrow[i + 1];
+                        if (bookcopy.containsKey(bid)) {
+                            Library l2 = bookcopy.get(bid);
+                            System.out.println(bid + " " + l2.bookName);
+                        }
+                    }
+                }
+                break;
+            case "3":
+                int count1 = 0, count2 = 0;
+                for (Library x : books.values()) {
+                    String s0 = x.bookName;
+                    if (s0.equalsIgnoreCase(borrowedUsers))
+                        ++count1;
+                }
+                for (Library x : bookcopy.values()) {
+                    String s0 = x.bookName;
+                    if (borrowedUsers.equalsIgnoreCase(s0))
+                        ++count2;
+                }
+                int bor = count2 - count1;
+                System.out.print(bor + " out\n" + count1 + " in\n");
+                break;
+            case "4":
+                for (Library x : bookcopy.values()) {
+                    String s0 = x.bookName;
+                    String s1 = x.author;
+                    if (s1.equals(borrowedUsers)) {
+                        System.out.println(s0);
+                    }
+                }
+                break;
+            case "5":
+                for (Library x : bookcopy.values()) {
+                    String s0 = x.bookName;
+                    String s = borrowedUsers.toLowerCase();
+                    if (s0.toLowerCase().contains(s)) {
+                        for (int i = 0;  i < ((bookcopy.size()) * 3);  i++) {
+                            if (book[i].equalsIgnoreCase(s0))
+                                System.out.println(book[i-1] + " " + s0);
+                        }
+                    }
+                }
+                break;
+        }
         return null;
     }
-
 }
 
 
--- src/Maintest.java
+++ src/Maintest.java
@@ -1,19 +1,15 @@
 public class Maintest {
 
     public static void main(String[] args) {
-        String booksInLibrary="125,C programming,Brian W. Kernighan|"
-                + "546,Java programming,James Gosling|"
-                + "897, Data Structure,Adam Drozdek|"
-                + "265, Data Structure,Adam Drozdek|"
-                + "234,The Java Language Specification,James Gosling|";
-        String borrowedUsers="125,101|897,104|546,101";
+
+        String booksInLibrary = "1001,Adventures of Sherlock Holmes,Sir Arthur Conan Doyle|" +
+                "1002,Adventures of Tom Sawyer,The Mark Twain|1003,Alice in the Wonderland,Lewis Carroll|" +
+                "1004,All’s Well that Ends well,William Shakespeare|1005,As you like it,William Shakespeare|" +
+                "1006,Anand Math,Bankim Chandra Chatterjee";
+        String borrowedUsers = "1006,101|1003,102|1001,101";
         Library l = new Library();
         l.createLibraryMap(booksInLibrary);
         l.createUserMap(borrowedUsers);
-        //l.getQuery(booksInLibrary,"546","1");
         l.getQuery(booksInLibrary,"101","2");
-        //l.getQuery(booksInLibrary,"Data Structures","3");
-        //l.getQuery(booksInLibrary,"James Gosling","4");
-        //l.getQuery(booksInLibrary,"java","5");
     }
 }
